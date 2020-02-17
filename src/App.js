73e7d1ce13e6ce9165eda3739324e370a9a9f8e7
dazzstone <senior.schapowal2015@gmail.com>
2020-02-17 23:01:12 +0200

    Added react-router-dom Added Messages page Renamed ContentBlock to Profile Added routes to Profile and Messages pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 import HeaderBlock from './components/HeaderBlock/index';
 import Sidebar from './components/Sidebar/index';
-import ContentBlock from './components/ContentBlock/index';
+import Profile from './components/Profile/index';
+import Messages from './components/Messages';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="main">
-      <HeaderBlock />
-      <Sidebar />
-      <ContentBlock />
-    </div>
+    <BrowserRouter>
+      <div className="main">
+        <HeaderBlock />
+        <Sidebar />
+        <div className='content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/messages' component={Messages} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { NavLink } from 'react-router-dom';
 
 const HeaderBlock = () => {
-    return(
+    return (
         <header className={styles.header}>
-            <img className={styles.img} src='https://cdn.pixabay.com/photo/2016/01/19/07/35/social-1148031_960_720.png' alt=''></img>
+            <NavLink to='/profile'>
+                <img className={styles.img} src='https://cdn.pixabay.com/photo/2016/01/19/07/35/social-1148031_960_720.png' alt=''></img>
+            </NavLink>
         </header>
     );
 }
